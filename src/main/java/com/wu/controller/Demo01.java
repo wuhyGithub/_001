3241472694bd0d2dbdc44c2226546a8d1ce9097c
wuhy <727773529@qq.com>
2021-07-18 11:29:24 +0800

    first upload project

---
+++ src/main/java/com/wu/controller/Demo01.java
@@ -0,0 +1,21 @@
+package com.wu.controller;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class Demo01 {
+
+    private String name;
+
+    public static void main(String[] args) {
+        List<String> list = new ArrayList<String>();
+        list.add("hello");
+        list.add("hello world");
+        System.out.print("test");
+        list.add("world");
+
+    }
+
+}
